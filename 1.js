d0239d2f41bbe4af7a8b86796d50dd381f91fd58
Yeongi Park <yeongi0111@naver.com>
2024-09-28 14:37:02 +0900

    데베응 3주차 과제 완료

--- 1.js
+++ 1.js
@@ -1,11 +1,55 @@
-const shuffle = (array) => {
-  let result = [];
-  while (array.length) {
-    let len = array.length;
-    let randomIndex = Math.floor(Math.random() * len);
-    result.push(...array.splice(randomIndex, 1));
+class Department {
+  constructor(department) {
+    this.department = department;
+    this.courses = [];
   }
-  return result;
-};
 
-console.log(shuffle([1, 2, 3, 4, 5, 6, 7, 8, 9]));
+  add_course(name, grade) {
+    const course = new Course(name, grade);
+    this.courses.push(course);
+    return course;
+  }
+
+  toString() {
+    return `학과 이름 = ${this.department}, 개설 과목 = [${this.courses.map(
+      (course) => `(${course.name}, ${course.grade})`
+    )}]`;
+  }
+}
+
+class Course {
+  constructor(name, grade) {
+    this.name = name;
+    this.grade = grade;
+  }
+}
+
+class Student {
+  constructor(name, studentNum) {
+    this.name = name;
+    this.studentNum = studentNum;
+    this.courses = [];
+  }
+
+  enroll(course) {
+    this.courses.push(course);
+  }
+
+  toString() {
+    return `학생 이름 = ${this.name}, 학번 = ${
+      this.studentNum
+    }, 수강 과목 = [${this.courses.map(
+      (course) => `(${course.name}, ${course.grade})`
+    )}]`;
+  }
+}
+
+const dept = new Department("컴퓨터공학과");
+const c1 = dept.add_course("대학생활의설계", 1);
+const c2 = dept.add_course("데이터베이스", 3);
+const c3 = dept.add_course("웹프로그래밍", 3);
+console.log(String(dept));
+const st = new Student("홍길동", 2020001);
+st.enroll(c2);
+st.enroll(c3);
+console.log(String(st));
--- 2.js
+++ 2.js
@@ -1,5 +1,15 @@
-const union = (arr1, arr2) => {
-  return [...new Set([...arr1, ...arr2])].sort((a, b) => a - b);
-};
+const urls = [
+  "https://jsonplaceholder.typicode.com/todos/1",
+  "https://jsonplaceholder.typicode.com/todos/2",
+  "https://jsonplaceholder.typicode.com/todos/3",
+];
 
-console.log(union([5, 6, 1, 3], [1, 8, 9, 5]));
+async function parallel_fetches(urls) {
+  const responses = await Promise.all(urls.map((url) => fetch(url)));
+  const data = await Promise.all(responses.map((res) => res.json()));
+  return data;
+}
+
+parallel_fetches(urls)
+  .then((contents) => console.log("Downloaded contents: ", contents))
+  .catch((error) => console.error(error));
